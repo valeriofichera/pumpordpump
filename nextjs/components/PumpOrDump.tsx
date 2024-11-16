f90bb926fa08714c456dd9c68392425fae0cee63
shafu <shafu0x@gmail.com>
2024-11-16 19:11:59 +0700

    refactor data

--- nextjs/components/PumpOrDump.tsx
+++ nextjs/components/PumpOrDump.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import Image from "next/image";
+import DB from "../data/db";
 import DumpImage from "./assets/DumpImage";
 import PumpImage from "./assets/PumpImage";
 import SuperPumpImage from "./assets/SuperPumpImage";
@@ -10,160 +11,15 @@ import TinderDrawer from "./tinder-drawer";
 import "animate.css";
 import TinderCard from "react-tinder-card";
 
-export const db = [
-  {
-    name: "bitcoin",
-    ticker: "BTC",
-    description: "Bitcoin is a decentralized digital currency, without a central bank or single administrator.",
-    mcap: "1.2T",
-    created: "2009",
-    price: "50k",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
-  },
-  {
-    name: "ethereum",
-    ticker: "ETH",
-    description: "Ethereum is a decentralized platform that enables smart contracts and decentralized applications.",
-    mcap: "400B",
-    created: "2015",
-    price: "3k",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
-  },
-  {
-    name: "solana",
-    ticker: "SOL",
-    description: "Solana is a high-performance blockchain supporting builders around the world creating crypto apps.",
-    mcap: "100B",
-    created: "2020",
-    price: "200",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
-  },
-  {
-    name: "usdc",
-    ticker: "USDC",
-    description: "USD Coin (USDC) is a type of cryptocurrency that is referred to as a stablecoin.",
-    mcap: "50B",
-    created: "2020",
-    price: "1",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
-  },
-  {
-    name: "dodge",
-    ticker: "DOGE",
-    description: "Dogecoin is a cryptocurrency that started as a joke but has gained a large following.",
-    mcap: "20B",
-    created: "2020",
-    price: "0.3",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png",
-  },
-  {
-    name: "Shibe",
-    ticker: "SHIB",
-    description:
-      "ShibeCoin is a meme-based cryptocurrency inspired by Shiba Inu dogs, aiming to bring joy and laughter to the crypto world.",
-    mcap: "15B",
-    created: "2021",
-    price: "0.00007",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5994.png",
-  },
-  {
-    name: "Fluffy",
-    ticker: "FLUF",
-    description:
-      "FluffyCoin is a fluffy meme coin created for crypto enthusiasts who believe in lighthearted, soft currency.",
-    mcap: "5B",
-    created: "2022",
-    price: "0.02",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5995.png",
-  },
-  {
-    name: "Wagmi",
-    ticker: "WAGMI",
-    description:
-      "WAGMICoin represents the optimism of the crypto world, reminding everyone 'We Are All Gonna Make It!'",
-    mcap: "3B",
-    created: "2020",
-    price: "0.5",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5996.png",
-  },
-  {
-    name: "Pepe",
-    ticker: "PEPE",
-    description:
-      "PepeCoin is a tribute to internet culture and meme lovers, featuring everyone's favorite frog as its mascot.",
-    mcap: "10B",
-    created: "2019",
-    price: "0.001",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5997.png",
-  },
-  {
-    name: "MemeGold",
-    ticker: "MGOLD",
-    description:
-      "MemeGold is a parody coin that shines with meme energy, bringing value to jokes and humor in the digital economy.",
-    mcap: "8B",
-    created: "2021",
-    price: "0.05",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5998.png",
-  },
-  {
-    name: "LaughCoin",
-    ticker: "LAUGH",
-    description:
-      "LaughCoin is the currency for sharing laughs, bringing lightheartedness to the serious world of crypto.",
-    mcap: "500M",
-    created: "2023",
-    price: "0.007",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/5999.png",
-  },
-  {
-    name: "FelineCoin",
-    ticker: "CAT",
-    description: "FelineCoin purrs its way through the crypto world, charming cat lovers with its adorable branding.",
-    mcap: "2B",
-    created: "2022",
-    price: "0.003",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/6000.png",
-  },
-  {
-    name: "RocketPup",
-    ticker: "RPUP",
-    description:
-      "RocketPup is a coin that aims for the moon, literally, as it gains popularity among meme enthusiasts.",
-    mcap: "7B",
-    created: "2020",
-    price: "0.04",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/6001.png",
-  },
-  {
-    name: "Cheems",
-    ticker: "CHEEMS",
-    description: "CheemsCoin is inspired by the famous 'Cheems' dog meme, offering a fun token for all meme fans.",
-    mcap: "6B",
-    created: "2019",
-    price: "0.0003",
-    url: "https://s2.coinmarketcap.com/static/img/coins/64x64/6002.png",
-  },
-  {
-    name: "BananaCoin",
-    ticker: "BNNA",
-    description: "BananaCoin is the crypto for monkeying around, designed to reward the silliest traders out there.",
-    mcap: "1B",
-    created: "2022",
-    price: "0.09",
-    url: "https://ichef.bbci.co.uk/ace/standard/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp",
-  },
-];
-
 export function PumpOrDump() {
-  const [currentIndex, setCurrentIndex] = useState<number>(db.length - 1);
+  const [currentIndex, setCurrentIndex] = useState<number>(DB.length - 1);
   const [lastDirection, setLastDirection] = useState<string | undefined>();
   const [showImage, setShowImage] = useState<boolean>(false);
   const currentIndexRef = useRef<number>(currentIndex);
 
   const childRefs = useMemo(
     () =>
-      Array(db.length)
+      Array(DB.length)
         .fill(0)
         .map(() => React.createRef<any>()),
     [],
@@ -174,7 +30,7 @@ export function PumpOrDump() {
     currentIndexRef.current = val;
   };
 
-  const canGoBack = currentIndex < db.length - 1;
+  const canGoBack = currentIndex < DB.length - 1;
   const canSwipe = currentIndex >= 0;
 
   const swiped = (direction: string, nameToDelete: string, index: number) => {
@@ -192,7 +48,7 @@ export function PumpOrDump() {
   };
 
   const swipe = async (dir: string) => {
-    if (canSwipe && currentIndex < db.length) {
+    if (canSwipe && currentIndex < DB.length) {
       await childRefs[currentIndex].current?.swipe(dir);
     }
   };
@@ -226,7 +82,7 @@ export function PumpOrDump() {
         <div>Pump Or Dump</div>
       </div>
       <div className="fixed left-0 right-0 flex justify-center items-center pt-96">
-        {db.map((character, index) => (
+        {DB.map((character, index) => (
           <TinderCard
             ref={childRefs[index]}
             className="swipe"
